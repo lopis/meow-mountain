08b6af77de2da6fcdb9fa052a762e619e45c4f4e
João Lopes <joaopclopes@gmail.com>
2025-08-29 15:40:33 +0200

    add music stuff

---
+++ src/core/audio.ts
@@ -0,0 +1,25 @@
+const t = (i: number, n: number)=>(n-i)/n;
+
+// Sound player
+export const playSound = (f: (i: number) => number) => {
+  const A = new AudioContext();
+  const m = A.createBuffer(1,96e3,48e3);
+  const b = m.getChannelData(0);
+  for(let i = 96e3; i--;) b[i] = f(i);
+  const s = A.createBufferSource();
+  s.buffer=m;
+  s.connect(A.destination);
+  s.start();
+};
+
+// Sound
+export const ooof = (pitch: number) => playSound((i: number) => {
+  var n=2e4;
+  if (i > n) return 0;
+  var q = t(i,n);
+  return 0.2 * Math.tan(Math.cbrt(Math.sin(i/(145 - 5 * pitch))))*q*q;
+});
+
+export const doorSound = () => playSound((i: number) => {
+  return 0.1 * Math.sin(i/50 + Math.random()*50) * (8000 - i%8000) / 5000 * Math.exp(-i/8000);
+});
---
+++ src/core/music-player.ts
@@ -0,0 +1,33 @@
+class MusicPlayer {
+  audioContext: AudioContext | undefined;
+  startTime = 0;
+  isPlaying = false;
+  buffer: AudioBuffer | undefined;
+  musicProcessorNode: AudioWorkletNode | undefined;
+
+  async start() {
+    if (this.isPlaying) return;
+
+    this.audioContext = new (window.AudioContext || (window as any).webkitAudioContext)();
+    await this.audioContext.resume();
+    await this.audioContext.audioWorklet.addModule('music-worklet.js');
+
+    this.musicProcessorNode = new AudioWorkletNode(this.audioContext, "music-processor");
+    
+    this.musicProcessorNode.connect(this.audioContext.destination);
+    this.isPlaying = true;
+  }
+
+  stop() {
+    if (this.isPlaying) {
+      this.musicProcessorNode?.disconnect();
+      this.isPlaying = false;
+    }
+  }
+
+  speedUp() {
+    this.musicProcessorNode?.port.postMessage({ name: "speed-up" });
+  }
+}
+
+export default new MusicPlayer();
